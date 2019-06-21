aca6d8de53d6105c50becfa8817c6bd1b07b015d
Jessica Pope <jessica.pope.co@gmail.com>
2019-06-21 10:10:19 -0600

    Styling, fetching correcg user and vacay

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const ulwel = document.querySelector(".welcome")
 const ul =  document.querySelector(".vacations")
 const ulvac = document.querySelector(".vacationdata")
 const dropDown = document.querySelector(".dropdown-content")
-
+const alpha = document.querySelector(".alpha")
 const huser = document.querySelector(".usertitle")
 const hvacation = document.querySelector(".vacationname")
 const userPage = document.querySelector('.userpage')
@@ -17,59 +17,82 @@ const vacationPage = document.querySelector('.vacationpage')
 const returningUserButton = document.querySelector('.returninguser')
 const addRowButton = document.querySelector("#add-row")
 const submitNewRowButton = document.querySelector("#submitRow")
-
+const homeButton = document.querySelector(".home")
 // fetchVacationNames()
 // fetchVacation1()
 // fetchUserNames()
 // fetchUser1()
 // fetchEvents()
+welcome()
 fetchUserNames()
+
 
 // returningUserButton.addEventListener("click", function(e){
 //   fetchUserNames()
 // })
 
-
+  function welcome(){
+    vacationPage.style.display = "none";
+    userPage.style.display = "none";
+    welcomePage.style.display = "block";
+  }
     function fetchUserNames(){
-        return fetch(userNames)
+        fetch(userNames)
         .then(response => response.json())
         .then(response => {
-          response.map(function(ce){
-          userId = ce.id
-          li = document.createElement('li')
-          li.textContent = ce.name
-          li.addEventListener("click", function(e){
-            fetchUser()
-          })
+          response.forEach(function(ce){
+            // userId = ce.id
+
+            li = document.createElement('li')
+            li.textContent = ce.name
+            console.log("first block",  ce.id)
+            li.addEventListener("click", function(e){
+              fetchUser(ce.id)
+
+            })
+
           dropDown.appendChild(li)
+
         })
       })
     }
 
-    function fetchUser(){
-      welcomePage.innerHTML = userPage
-      huser.innerText = "Your Vacations!"
+    function fetchUser(userId){
+      welcomePage.style.display = "none";
+      vacationPage.style.display = "none";
+      userPage.style.display = "none";
+      userPage.style.display = "block";
+
       const userShow = `http://localhost:3000/users/${userId}`
-        return fetch(userShow)
+          fetch(userShow)
         .then(response => response.json())
         .then(response => {
-          response.vacations.map(function(e){
-          vacationId = e.id
+          console.log(response)
+          response.vacations.map(function(ec){
+          vacationId = ec.id
+          console.log("vacation id",vacationId)
           liea = document.createElement('li')
-          liea.textContent = e.name
+          liea.textContent = ec.name
           liea.addEventListener("click", function(e){
-            fetchVacation()
+            fetchVacation(ec.id)
+            console.log("fetch vacation",ec.id)
           })
           ul.appendChild(liea)
           })
           })
         }
 
-function fetchVacation(){
-  userPage.innerHTML = vacationPage
+function fetchVacation(vacationId){
+  vacationPage.style.display = "block";
+  userPage.style.display = "none";
+  welcomePage.style.display = "none";
+
+
+    userPage.style.display = "none";
+    vacationPage.style.display = "block";
   hvacation.innerText = "This is the vacation data page!"
   addRowButton.innerText = "Add Row"
-    const vacationShow = "http://localhost:3000/vacations/1"
+    const vacationShow = `http://localhost:3000/vacations/${vacationId}`
       return fetch(vacationShow)
       .then(response => response.json())
       .then(response => {
@@ -79,12 +102,17 @@ function fetchVacation(){
          	data:response.events, //assign data to table
          	layout:"fitColumns", //fit columns to width of table (optional)
          	columns:[ //Define Table Columns
-        	 	{title:"Name", field:"name", width:150, editor:"textarea"},
+        	 	{title:"Name", field:"name", width:500, editor:"textarea"},
         	 	{title:"Amount", field:"amount", width:150, editor:"number"},
             {title:"User", field:"user_id", width:150, editor:"number"},
-        	 	{title:"Category", field:"category", width:150, editor:"textarea"},
-        	 	{title:"Date Of Event", field:"date_of_event", editor:"textarea", sorter:"date", align:"center"},
-            {title:"", field:"vacation_id", width:50, editor:"number"}
+        	 	{title:"Category", field:"category", width:200, editor:"textarea"},
+        	 	{title:"Date Of Event", field:"date_of_event", width:200, editor:"textarea", sorter:"date", align:"center"},
+            {title:"VacId", field:"vacation_id", width:100, editor:"number"},
+            {formatter:"buttonCross", align:"center", width:100, title:"del", headerSort:false, cellClick:function(e, cell){
+	if(confirm('Are you sure you want to delete this entry?'))
+		cell.getRow().delete();
+	}
+}
          	],
          	// rowClick:function(e, row){ //trigger an alert message when the row is clicked
          	// 	alert("Row " + row.getData().id + " Clicked!!!!");
@@ -138,6 +166,7 @@ function fetchVacation(){
                        })
                 }
           }
+
         })
         addRowButton.addEventListener('click', function(){
           table.addRow({vacation_id:1})
@@ -145,22 +174,13 @@ function fetchVacation(){
 
         const data = table.getData()
         console.log(data)
+          })
+        }
 
+        homeButton.addEventListener('click',function(){
 
-
-
-
-
-
-
-
-
-        })
-
-
-  }
-
-
+          welcome()
+            })
 
 
 
